68c9fdee9032ff3032e802ead8ba80d14f968a97
LICF01 <lucascubilla@gmail.com>
2023-04-07 14:12:12 -0400

    delete plugins imports

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -18,27 +18,9 @@ return {
 	-- commenting with gc
 	"numToStr/Comment.nvim",
 
-	-- snippets
-	"L3MON4D3/LuaSnip", -- snippet engine
-	"rafamadriz/friendly-snippets", -- useful snippets
-
-	-- configuring lsp servers
-	"hrsh7th/cmp-nvim-lsp", -- for autocompletion
-	{ "glepnir/lspsaga.nvim", branch = "main" }, -- enhanced lsp uis
+	-- lsp related
 	"jose-elias-alvarez/typescript.nvim", -- additional functionality for typescript server (e.g. rename file & update imports)
 	"onsails/lspkind.nvim", -- vs-code like icons for autocompletion
-
-	-- treesitter configuration
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-	},
 
 	-- auto closing
 	{ "windwp/nvim-ts-autotag", dependencies = "nvim-treesitter" }, -- autoclose tags
